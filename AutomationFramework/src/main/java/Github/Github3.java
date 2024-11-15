3583b24f122475cf0e2040a8ebd2640a2454df36
abhinav <abhinav@ABHINAV-PC>
2024-11-15 13:50:52 +0530

    hello

---
+++ AutomationFramework/src/main/java/Github/Github3.java
@@ -0,0 +1,12 @@
+package Github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Github3 {
+	@Test
+	public void demo() {
+		Reporter .log("newest code",true);
+	}
+
+}
