13e553ac747fb674d4b53a4141b9ed592bb3bdb8
abhinav <abhinav@ABHINAV-PC>
2024-11-19 12:14:24 +0530

    hi

---
+++ AutomationFramework/src/test/java/login/Login.java
@@ -0,0 +1,12 @@
+package login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Login {
+	@Test
+	public void login() {
+		Reporter.log("login succesful",true);
+	}
+
+}
